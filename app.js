52437511e0db4f3ca2c3a3ea6ac597c07a4f2867
wkdtjsgn <ekdtjsgn@naver.com>
2022-05-25 11:00:37 +0900

    pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('wkdtjsgn-CICD-change'));
+app.get('/', (req, res) => res.send('wkdtjsgn-CICD-change-2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
